a38ef833fd61b734c385cd99204c1a43847f0d1a
katerine1234 <ivanandresbernalhernandez595@gmail.com>
2023-08-31 16:02:39 -0500

    Cargar repositorio

---
+++ dogs/src/packet/Dogs.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package packet;
+
+/**
+ *
+ * @author Katerine Forero
+ * Esto nos lleva al inglés que es el idioma predominante en la comunidad de desarrollo de software y 
+ * se utiliza en la mayoría de la documentación, recursos en línea, tutoriales y discusiones en foros.
+ * fuente: blog.desafiolatam.com
+ * 
+ */
+public class Dogs {
+
+    
+    public static void main(String[] args) {
+     new SmallInterface ().setVisible(true);
+     
+    }
+    
+}
